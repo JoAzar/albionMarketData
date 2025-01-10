178975de7f7836575313486b3344f533a4bbc012
JoAzar <joel.zalazar.trabajo@gmail.com>
2025-01-10 11:27:08 -0300

    cambios viernes 10 enero 2025 10:28hs

---
+++ productos.py
@@ -0,0 +1,51 @@
+#DICCIONARIO DE PRODUCTOS
+
+productos = {
+    "Peces": {
+        "Peces Comunes T2":"T2_FISH_FRESHWATER_ALL_COMMON",
+        "Peces Comunes T3":"T3_FISH_FRESHWATER_ALL_COMMON",
+        "Peces Comunes T4":"T4_FISH_FRESHWATER_ALL_COMMON",
+        "Almeja T3":"T3_FISH_FRESHWATER_SWAMP_RARE",
+        "Almeja T5":"T5_FISH_FRESHWATER_SWAMP_RARE",
+        "Almeja T7":"T7_FISH_FRESHWATER_SWAMP_RARE",
+        "Ajolote T3":"T3_FISH_FRESHWATER_HIGHLANDS_RARE",
+        "Ajolote T5":"T5_FISH_FRESHWATER_HIGHLANDS_RARE",
+        "Ajolote T7":"T7_FISH_FRESHWATER_HIGHLANDS_RARE",
+    },
+    "Joyas": {
+        "Treasure Knowledge": "TREASURE_KNOWLEDGE_RARITY1",
+        "Treasure Silverware": "TREASURE_SILVERWARE_RARITY1",
+        "Treasure Decorative": "TREASURE_DECORATIVE_RARITY1",
+        "Treasure Tribal": "TREASURE_TRIBAL_RARITY1",
+        "Treasure Ritual": "TREASURE_RITUAL_RARITY1",
+        "Treasure Ceremonial": "TREASURE_CEREMONIAL_RARITY1",
+    },
+     "Producto T2": {
+        "Madera": "T2_WOOD",
+        "Minerales": "T2_ORE",
+        "Piedra": "T2_ROCK",
+        "Cuero": "T2_HIDE",
+        "Fibra": "T2_FIBER"
+    },
+    "Producto T3": {
+        "Madera": "T3_WOOD",
+        "Minerales": "T3_ORE",
+        "Piedra": "T3_ROCK",
+        "Cuero": "T3_HIDE",
+        "Fibra": "T3_FIBER"
+    },
+    "Producto T4": {
+        "Madera": "T4_WOOD",
+        "Minerales": "T4_ORE",
+        "Piedra": "T4_ROCK",
+        "Cuero": "T4_HIDE",
+        "Fibra": "T4_FIBER"
+    },
+    "Producto T5": {
+        "Madera": "T5_WOOD",
+        "Minerales": "T5_ORE",
+        "Piedra": "T5_ROCK",
+        "Cuero": "T5_HIDE",
+        "Fibra": "T5_FIBER"
+    }
+}
