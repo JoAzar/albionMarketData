ad2414fb0c891662adfbb8a2acdce835456c5a80
red <red@Skynet.Skynet>
2025-01-11 02:17:18 -0300

    cambios 11 de enero 2025 2:17am

--- main.py
+++ main.py
@@ -4,18 +4,29 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import streamlit as st
 from productos import productos
+import requests
+
+
 
 st.title("Comparativa de Precios en Albion Online")
 
 #Selección del producto
-option = st.selectbox("Seleccione el producto:", ["Peces","Joyas","Producto T2", "Producto T3", "Producto T4", "Producto T5"])
+option = st.selectbox("Seleccione el producto:", [
+    "Peces","Joyas","Producto T2", "Producto T3", 
+    "Producto T4", "Producto T4 1", "Producto T4 2", "Producto T4 3", "Producto T4 4", 
+    "Producto T5", "Producto T5 1", "Producto T5 2", "Producto T5 3", "Producto T5 4",
+    "Producto T6", "Producto T6 1", "Producto T6 2", "Producto T6 3", "Producto T6 4",
+    "Producto T7", "Producto T7 1", "Producto T7 2", "Producto T7 3", "Producto T7 4",
+    "Producto T8", "Producto T8 1", "Producto T8 2", "Producto T8 3", "Producto T8 4"
+    ])
 
 #Selección del ítem dentro del producto
 item_option = st.selectbox("Seleccione el ítem:", list(productos[option].keys()))
 item_id = productos[option][item_option]
 
 #Selección de la región
-region = st.selectbox("Seleccione la región:", ["europe", "asia", "west"])
+region = st.selectbox("Seleccione la región:", ["europe", "west", "east"])
+
 url = f"https://{region}.albion-online-data.com/api/v2/stats/prices/{item_id}"
 
 # Construir la URL de la imagen
@@ -45,31 +56,63 @@ if st.button("Consultar Precios"):
         df_sell = pd.DataFrame({"Ciudad": cities_sell, "Precio Venta": sell_prices})
         df_buy = pd.DataFrame({"Ciudad": cities_buy, "Precio Compra": buy_prices})
 
-        #Mostrar tablas
-        st.subheader("Precios de Venta")
-        st.dataframe(df_sell)
+        col1, col2 = st.columns(2)
 
-        st.subheader("Precios de Compra")
-        st.dataframe(df_buy)
+        with col1:
+            st.write("Precio de Venta")
+            st.dataframe(df_sell)
 
-        #Gráfico de precios de Venta
+        with col2:
+            st.write("Precio de Compra")
+            st.dataframe(df_buy)
+
+        # Configuración de estilo de Seaborn sin fondo
+        sns.set(style="ticks", palette="muted")
+
+        # Gráfico de precios de Venta
         if not df_sell.empty:
-            plt.figure(figsize=(10, 6))
-            sns.barplot(x='Ciudad', y='Precio Venta', data=df_sell, color='skyblue')
-            plt.title(f'Precios de Venta por Ciudad - {item_id}')
-            plt.xlabel('Ciudad')
-            plt.ylabel('Precio de Venta')
-            plt.xticks(rotation=45)
+            plt.figure(figsize=(12, 8))
+            ax = sns.barplot(x='Ciudad', y='Precio Venta', data=df_sell, color='skyblue')
+            
+            # Mejorar el título, etiquetas y formato
+            ax.set_title(f'Precios de Venta por Ciudad - {item_id}', fontsize=16, fontweight='bold', color='blue', pad=20)
+            ax.set_xlabel('Ciudad', fontsize=14, fontweight='bold', color='darkgray')
+            ax.set_ylabel('Precio de Venta', fontsize=14, fontweight='bold', color='darkgray', labelpad=20)
+
+            #ejes
+            plt.xticks(rotation=45, ha='right', fontsize=12, color='white')
+            plt.yticks(fontsize=12, color='white')
+
+            # Añadir anotaciones con los valores de las barras
+            for p in ax.patches:
+                ax.annotate(f'{p.get_height():.2f}', (p.get_x() + p.get_width() / 2., p.get_height()),
+                ha='center', va='center', fontsize=12, color='white', xytext=(0, 10), textcoords='offset points')
+
+            plt.gcf().patch.set_facecolor('none')
+            ax.set_facecolor('none')
             st.pyplot(plt)
 
-        #Gráfico de precios de Compra
+        # Gráfico de precios de Compra
         if not df_buy.empty:
-            plt.figure(figsize=(10, 6))
-            sns.barplot(x='Ciudad', y='Precio Compra', data=df_buy, color='orange')
-            plt.title(f'Precios de Compra por Ciudad - {item_id}')
-            plt.xlabel('Ciudad')
-            plt.ylabel('Precio de Compra')
-            plt.xticks(rotation=45)
+            plt.figure(figsize=(12, 8))
+            ax = sns.barplot(x='Ciudad', y='Precio Compra', data=df_buy, color='orange')
+            
+            # Mejorar el título, etiquetas y formato
+            ax.set_title(f'Precios de Compra por Ciudad - {item_id}', fontsize=16, fontweight='bold', color='orange', pad=20)
+            ax.set_xlabel('Ciudad', fontsize=14, fontweight='bold', color='darkgray')
+            ax.set_ylabel('Precio de Compra', fontsize=14, fontweight='bold', color='darkgray', labelpad=20)
+
+            #ejes
+            plt.xticks(rotation=45, ha='right', fontsize=12, color='white')
+            plt.yticks(fontsize=12, color='white')
+
+            for p in ax.patches:
+                ax.annotate(f'{p.get_height():.2f}', (p.get_x() + p.get_width() / 2., p.get_height()),
+                ha='center', va='center', fontsize=12, color='white', xytext=(0, 10), textcoords='offset points')
+
+            plt.gcf().patch.set_facecolor('none')
+            ax.set_facecolor('none')
+
             st.pyplot(plt)
     else:
         st.warning("No se encontraron datos para el ítem seleccionado.")
--- productos.py
+++ productos.py
@@ -41,11 +41,173 @@ productos = {
         "Cuero": "T4_HIDE",
         "Fibra": "T4_FIBER"
     },
+    "Producto T4 1": {
+        "Madera": "T4_WOOD_LEVEL1",
+        "Minerales": "T4_ORE_LEVEL1",
+        "Piedra": "T4_ROCK_LEVEL1",
+        "Cuero": "T4_HIDE_LEVEL1",
+        "Fibra": "T4_ FIBER_LEVEL1"
+    },
+    "Producto T4 2": {
+        "Madera": "T4_WOOD_LEVEL2",
+        "Minerales": "T4_ORE_LEVEL2",
+        "Piedra": "T4_ROCK_LEVEL2",
+        "Cuero": "T4_HIDE_LEVEL2",
+        "Fibra": "T4_ FIBER_LEVEL2"
+    },
+    "Producto T4 3": {
+        "Madera": "T4_WOOD_LEVEL3",
+        "Minerales": "T4_ORE_LEVEL3",
+        "Piedra": "T4_ROCK_LEVEL3",
+        "Cuero": "T4_HIDE_LEVEL3",
+        "Fibra": "T4_ FIBER_LEVEL3"
+    },
+    "Producto T4 4": {
+        "Madera": "T4_WOOD_LEVEL4",
+        "Minerales": "T4_ORE_LEVEL4",
+        "Piedra": "T4_ROCK_LEVEL4",
+        "Cuero": "T4_HIDE_LEVEL4",
+        "Fibra": "T4_ FIBER_LEVEL4"
+    },
     "Producto T5": {
         "Madera": "T5_WOOD",
         "Minerales": "T5_ORE",
         "Piedra": "T5_ROCK",
         "Cuero": "T5_HIDE",
         "Fibra": "T5_FIBER"
-    }
+    },
+    "Producto T5 1": {
+        "Madera": "T5_WOOD_LEVEL1",
+        "Minerales": "T5_ORE_LEVEL1",
+        "Piedra": "T5_ROCK_LEVEL1",
+        "Cuero": "T5_HIDE_LEVEL1",
+        "Fibra": "T5_FIBER_LEVEL1"
+    },
+    "Producto T5 2": {
+        "Madera": "T5_WOOD_LEVEL2",
+        "Minerales": "T5_ORE_LEVEL2",
+        "Piedra": "T5_ROCK_LEVEL2",
+        "Cuero": "T5_HIDE_LEVEL2",
+        "Fibra": "T5_FIBER_LEVEL2"
+    },
+    "Producto T5 3": {
+        "Madera": "T5_WOOD_LEVEL3",
+        "Minerales": "T5_ORE_LEVEL3",
+        "Piedra": "T5_ROCK_LEVEL3",
+        "Cuero": "T5_HIDE_LEVEL3",
+        "Fibra": "T5_FIBER_LEVEL3"
+    },
+    "Producto T5 4": {
+        "Madera": "T5_WOOD_LEVEL4",
+        "Minerales": "T5_ORE_LEVEL4",
+        "Piedra": "T5_ROCK_LEVEL4",
+        "Cuero": "T5_HIDE_LEVEL4",
+        "Fibra": "T5_FIBER_LEVEL4"
+    },
+    "Producto T6": {
+        "Madera": "T6_WOOD",
+        "Minerales": "T6_ORE",
+        "Piedra": "T6_ROCK",
+        "Cuero": "T6_HIDE",
+        "Fibra": "T6_FIBER"
+    },
+    "Producto T6 1": {
+        "Madera": "T6_WOOD_LEVEL1",
+        "Minerales": "T6_ORE_LEVEL1",
+        "Piedra": "T6_ROCK_LEVEL1",
+        "Cuero": "T6_HIDE_LEVEL1",
+        "Fibra": "T6_FIBER_LEVEL1"
+    },
+    "Producto T6 2": {
+        "Madera": "T6_WOOD_LEVEL2",
+        "Minerales": "T6_ORE_LEVEL2",
+        "Piedra": "T6_ROCK_LEVEL2",
+        "Cuero": "T6_HIDE_LEVEL2",
+        "Fibra": "T6_FIBER_LEVEL2"
+    },
+    "Producto T6 3": {
+        "Madera": "T6_WOOD_LEVEL3",
+        "Minerales": "T6_ORE_LEVEL3",
+        "Piedra": "T6_ROCK_LEVEL3",
+        "Cuero": "T6_HIDE_LEVEL3",
+        "Fibra": "T6_FIBER_LEVEL3"
+    },
+    "Producto T6 4": {
+        "Madera": "T6_WOOD_LEVEL4",
+        "Minerales": "T6_ORE_LEVEL4",
+        "Piedra": "T6_ROCK_LEVEL4",
+        "Cuero": "T6_HIDE_LEVEL4",
+        "Fibra": "T6_FIBER_LEVEL4"
+    },
+    "Producto T7": {
+        "Madera": "T7_WOOD",
+        "Minerales": "T7_ORE",
+        "Piedra": "T7_ROCK",
+        "Cuero": "T7_HIDE",
+        "Fibra": "T7_FIBER"
+    },
+    "Producto T7 1": {
+        "Madera": "T7_WOOD_LEVEL1",
+        "Minerales": "T7_ORE_LEVEL1",
+        "Piedra": "T7_ROCK_LEVEL1",
+        "Cuero": "T7_HIDE_LEVEL1",
+        "Fibra": "T7_FIBER_LEVEL1"
+    },
+    "Producto T7 2": {
+        "Madera": "T7_WOOD_LEVEL2",
+        "Minerales": "T7_ORE_LEVEL2",
+        "Piedra": "T7_ROCK_LEVEL2",
+        "Cuero": "T7_HIDE_LEVEL2",
+        "Fibra": "T7_FIBER_LEVEL2"
+    },
+    "Producto T7 3": {
+        "Madera": "T7_WOOD_LEVEL3",
+        "Minerales": "T7_ORE_LEVEL3",
+        "Piedra": "T7_ROCK_LEVEL3",
+        "Cuero": "T7_HIDE_LEVEL3",
+        "Fibra": "T7_FIBER_LEVEL3"
+    },
+    "Producto T7 4": {
+        "Madera": "T7_WOOD_LEVEL4",
+        "Minerales": "T7_ORE_LEVEL4",
+        "Piedra": "T7_ROCK_LEVEL4",
+        "Cuero": "T7_HIDE_LEVEL4",
+        "Fibra": "T7_FIBER_LEVEL4"
+    },
+    "Producto T8": {
+        "Madera": "T8_WOOD",
+        "Minerales": "T8_ORE",
+        "Piedra": "T8_ROCK",
+        "Cuero": "T8_HIDE",
+        "Fibra": "T8_FIBER"
+    },
+    "Producto T8 1": {
+        "Madera": "T8_WOOD_LEVEL1",
+        "Minerales": "T8_ORE_LEVEL1",
+        "Piedra": "T8_ROCK_LEVEL1",
+        "Cuero": "T8_HIDE_LEVEL1",
+        "Fibra": "T8_FIBER_LEVEL1"
+    },
+    "Producto T8 2": {
+        "Madera": "T8_WOOD_LEVEL2",
+        "Minerales": "T8_ORE_LEVEL2",
+        "Piedra": "T8_ROCK_LEVEL2",
+        "Cuero": "T8_HIDE_LEVEL2",
+        "Fibra": "T8_FIBER_LEVEL2"
+    },
+    "Producto T8 3": {
+        "Madera": "T8_WOOD_LEVEL3",
+        "Minerales": "T8_ORE_LEVEL3",
+        "Piedra": "T8_ROCK_LEVEL3",
+        "Cuero": "T8_HIDE_LEVEL3",
+        "Fibra": "T8_FIBER_LEVEL3"
+    },
+    "Producto T8 4": {
+        "Madera": "T8_WOOD_LEVEL4",
+        "Minerales": "T8_ORE_LEVEL4",
+        "Piedra": "T8_ROCK_LEVEL4",
+        "Cuero": "T8_HIDE_LEVEL4",
+        "Fibra": "T8_FIBER_LEVEL4"
+    },
+
 }
